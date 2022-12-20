03e6013246f2e15ba8bc6c621975cb8e2375d289
Camilo Osorio <brycos15@gmail.com>
2022-12-20 07:56:22 -0500

    Se verifica que el email ingresado en el login exista

--- controllers/auth.controllers.ts
+++ controllers/auth.controllers.ts
@@ -1,15 +1,44 @@
 import { Request, Response } from "express";
+import User from '../models/user';
 
 
-export const loginUser = (req: Request, res: Response) => {
+export const loginUser = async (req: Request, res: Response): Promise<void> => {
 
-    res.json({
-        msg: 'Login'
-    });
+
+    const { email, password } = req.body;
+
+    const { body } = req;
+
+    try {
+
+        const emailExist = await User.findOne({
+            where: {
+                email
+            }
+        });
+
+        //Validar que el correo exista en base de datos
+        if(!emailExist){
+            throw new Error('Correo o contraseña inexistente');
+        } else {
+            
+            res.json({
+                body: emailExist
+            })
+            
+        }
+        
+    } catch (error) {
+        
+    }
+
+    
 
 }
 
-export const registerUser = (req: Request, res: Response) => {
+export const registerUser = async (req: Request, res: Response): Promise<void> => {
+
+    const emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
 
     res.json({
         msg: 'Register'
@@ -17,7 +46,7 @@ export const registerUser = (req: Request, res: Response) => {
 
 }
 
-export const renewPassword = (req: Request, res: Response) => {
+export const renewPassword = async (req: Request, res: Response): Promise<void> => {
 
     res.json({
         msg: 'Renew'
--- dist/controllers/auth.controllers.js
+++ dist/controllers/auth.controllers.js
@@ -1,22 +1,53 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.renewPassword = exports.registerUser = exports.loginUser = void 0;
-const loginUser = (req, res) => {
-    res.json({
-        msg: 'Login'
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.renewPassword = exports.registerUser = exports.loginUser = void 0;
+const user_1 = __importDefault(require("../models/user"));
+const loginUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { email, password } = req.body;
+    const { body } = req;
+    try {
+        const emailExist = yield user_1.default.findOne({
+            where: {
+                email
+            }
+        });
+        //Validar que el correo exista en base de datos
+        if (!emailExist) {
+            throw new Error('Correo o contraseña inexistente');
+        }
+        else {
+            res.json({
+                body: emailExist
+            });
+        }
+    }
+    catch (error) {
+    }
+});
 exports.loginUser = loginUser;
-const registerUser = (req, res) => {
+const registerUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
     res.json({
         msg: 'Register'
     });
-};
+});
 exports.registerUser = registerUser;
-const renewPassword = (req, res) => {
+const renewPassword = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     res.json({
         msg: 'Renew'
     });
-};
+});
 exports.renewPassword = renewPassword;
 //# sourceMappingURL=auth.controllers.js.map
